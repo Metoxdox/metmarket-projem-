b57b5353b4b575be0b946e7c6bf34594cf730ab4
Metoxdox <72650527+Metoxdox@users.noreply.github.com>
2026-01-08 18:59:42 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,14 +1,13 @@
 <?php
-// 1. Initialize the session
+
 session_start();
 
-// 2. Unset all of the session variables
+
 $_SESSION = array();
 
-// 3. Destroy the session
 session_destroy();
 
-// 4. Redirect to the Login Page
+
 header("Location: login.php");
 exit;
 ?>
