23bc694490e17c32030fb24c93121892179ea3d4
Metoxdox <72650527+Metoxdox@users.noreply.github.com>
2026-01-08 18:57:49 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
-// db.php
+
 session_start();
 
 $host = 'localhost';
 $db   = 'grocery_db';
 $user = 'root';
-$pass = ''; // Default XAMPP password is empty
+$pass = ''; 
 
 $conn = new mysqli($host, $user, $pass, $db);
 
@@ -13,7 +13,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Helper function for security (XSS prevention)
+
 function escape($string) {
     global $conn;
     return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
